ea8c1b7cdc8831276db2e98d4b473bff501ba09a
sanjay singh <fta.sanjay@gmail.com>
2025-12-16 23:12:01 +0530

    rebase testing

--- demo.ts
+++ demo.ts
@@ -1,3 +1,4 @@
 console.log('Hello, World!');
 console.log('This is a demo file.');
 console.log('Demonstrating TypeScript code execution.');
+console.log('TypeScript is great for building scalable applications.');
