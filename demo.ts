26aeb8f099fe9da4596cb530ec37dbabb4a3a481
Sanjay Singh <83452312+fta-sanjay@users.noreply.github.com>
2025-12-17 08:48:55 +0530

    Comment out demo log statements
    Comment out the initial console log statements.

--- demo.ts
+++ demo.ts
@@ -1,5 +1,5 @@
-console.log('Hello, World!');
-console.log('This is a demo file.');
+// console.log('Hello, World!');
+// console.log('This is a demo file.');
 console.log('Demonstrating TypeScript code execution.');
 console.log('TypeScript is great for building scalable applications.');
 console.log('End of demo file.   hhhh');
